04792b03369e2dbb5b11ef7fbbbcd847bab7a619
Shi-Kwan <sktan@insytecg.com>
2018-01-25 23:25:08 -0500

    edited server js file2

--- server.js
+++ server.js
@@ -4,11 +4,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var js = require("js");
-var img = require("img");
-var css = require("css");
-var contactform = require("contactform");
-var lib = require("lib")
+
 
 // Sets up the Express App
 // =============================================================
@@ -18,11 +14,16 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname +'/contactForm'));
-app.use(express.static(__dirname + '/css'));
-app.use(express.static(__dirname + '/img'));
-app.use(express.static(__dirname + '/js'));
-app.use(express.static(__dirname + '/lib'));
+var contactFormPath = path.resolve(__dirname, 'contactForm');
+var cssPath = path.resolve(__dirname, 'css');
+var imgPath = path.resolve(__dirname, 'img');
+var jsPath = path.resolve(__dirname, 'js');
+var libPath = path.resolve(__dirname, 'lib');
+app.use(express.static(contactFormPath));
+app.use(express.static(cssPath));
+app.use(express.static(imgPath));
+app.use(express.static(jsPath));
+app.use(express.static(libPath));
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
