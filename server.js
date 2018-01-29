ea02f3ce3d72d512041b7d3e2e34a6e26f925d32
Shi-Kwan <sktan@insytecg.com>
2018-01-29 14:43:59 -0500

    changed the source directory

--- server.js
+++ server.js
@@ -14,16 +14,17 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-var contactFormPath = path.resolve(__dirname, 'contactForm');
+/* var contactFormPath = path.resolve(__dirname, 'contactForm');
 var cssPath = path.resolve(__dirname, 'css');
 var imgPath = path.resolve(__dirname, 'img');
 var jsPath = path.resolve(__dirname, 'js');
 var libPath = path.resolve(__dirname, 'lib');
 app.use(express.static(contactFormPath));
-app.use(express.static(cssPath));
+app.use(express.static(__dirname + '/css'));
 app.use(express.static(imgPath));
 app.use(express.static(jsPath));
-app.use(express.static(libPath));
+app.use(express.static(libPath)); */
+app.use(express.static('.'));
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
